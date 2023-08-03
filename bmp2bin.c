9aa4d94904d1e5a433368b42009daab6ca509419
berniechen <s99056020@gmail.com>
2023-08-03 21:32:03 +0800

    update

--- bmp2bin.c
+++ bmp2bin.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<error.h>
-
+#include<string.h>
+#define FORE_BYTES_TO_BITS	(32)
+#define FORE_BYTES_ALIGN	(4)
 //BMP file header structure
 #pragma pack(1)
 typedef struct {		//header size 14 byte
@@ -30,12 +33,14 @@ typedef struct {		//header size 40 byte
 static void usage()
 {
 	printf("usage:\n");
-	printf("bmp2bin <src.bmp> <output.bin>\n");
+	printf("bmp2bin <src.bmp>\n");
 	return;
 }
 int main(int argc, char *argv[])
 {
 	FILE *file;
+	unsigned char *imgData = NULL;
+	int bytesPerRow = 0, i = 0, j = 0, k = 0;
 	BMPFileHeader fileHeader;
 	BMPInfoHeader infoHeader;
 
@@ -53,32 +58,59 @@ int main(int argc, char *argv[])
 		printf("This is not BMP file\n");
 		return -1;
 	}
-
+	printf("header size:14 bytes\n");
 	printf("BMP header:\n");
 	printf("signature:	0x%04X\n", fileHeader.signature);
-	printf("BMP file size:	0x%04X\n", fileHeader.fileSize);
-	printf("reserved:	0x%04X\n", fileHeader.reserved);
+	printf("BMP file size:	  %d\n", fileHeader.fileSize);
+	printf("reserved:	  %d\n", fileHeader.reserved);
 	printf("dataOffset:	0x%04X\n", fileHeader.dataOffset);
 
 	fread(&infoHeader, sizeof(infoHeader), 1, file);
-unsigned int imageSize;     //image data length
-    int xPixelsPerMeter;
-    int yPixelsPerMeter;
-    unsigned int colorsUsed;
-    unsigned int importantColors;
 
+	printf("info size:40 bytes\n");
 	printf("\nInfo:\n");
-	printf("width:		0x%04X\n", infoHeader.width);
-	printf("height:		0x%04X\n", infoHeader.height);
-	printf("planes:		0x%02X\n", infoHeader.planes);
-	printf("bitsPerPixel:   0x%02X\n", infoHeader.bitsPerPixel);
-	printf("compression:	0x%04X\n", infoHeader.compression);
-	printf("imageSize:	0x%04X\n", infoHeader.imageSize);
-	printf("xPixelsPerMeter:%d\n", infoHeader.xPixelsPerMeter);
-	printf("yPixelsPerMeter:%d\n", infoHeader.yPixelsPerMeter);
-	printf("colorUsed:	0x%04X\n", infoHeader.colorsUsed);
-	printf("importantColors:0x%04X\n", infoHeader.importantColors);
+	printf("width:		  %d\n", infoHeader.width);
+	printf("height:		  %d\n", infoHeader.height);
+	printf("planes:		  %d\n", infoHeader.planes);
+	printf("bitsPerPixel:     %d\n", infoHeader.bitsPerPixel);
+	printf("compression:	  %d\n", infoHeader.compression);
+	printf("imageSize:	  %d\n", infoHeader.imageSize);
+	printf("xPixelsPerMeter:  %d\n", infoHeader.xPixelsPerMeter);
+	printf("yPixelsPerMeter:  %d\n", infoHeader.yPixelsPerMeter);
+	printf("colorUsed:	  %d\n", infoHeader.colorsUsed);
+	printf("importantColors:  %d\n", infoHeader.importantColors);
 
+	bytesPerRow = ((infoHeader.bitsPerPixel * infoHeader.width + 31) / FORE_BYTES_TO_BITS) * FORE_BYTES_ALIGN;
+
+	printf("\n%d bytes needed per one row\n\n", bytesPerRow);
+
+	imgData = (unsigned char *)malloc(infoHeader.imageSize);
+
+	fseek(file, fileHeader.dataOffset, SEEK_SET);
+
+	fread(imgData, sizeof(unsigned char), infoHeader.imageSize, file);
+
+	printf("\n%d raw bytes(little endian):\n", infoHeader.imageSize);
+	for(i = 0 ; i < infoHeader.imageSize ; i++) {
+		if(i > 0 && (i % bytesPerRow == 0))
+			printf("\n");
+		printf("0x%02X ", imgData[i]);
+	}
+
+	printf("\n\nlayout:\n");
+	unsigned int temp = 0;
+	unsigned int mask = 1;
+	for(i = 0 ; i < infoHeader.height ; i++) {
+		for(j = 0 ; j < bytesPerRow; j += FORE_BYTES_ALIGN) {
+			temp = (unsigned int)imgData[j];
+			for(k = 31 ; k >= 0 ; k--) {
+				temp >> k & mask ? printf("1 ") : printf("0 ");
+			}
+		}
+		printf("\n");
+	}
+
+	free(imgData);
 	fclose(file);
 	return 0;
 usage:
